ef64b6357681297a393f6bfaa227e2e5dbd1261e
Darith49 <212290175+Darith49@users.noreply.github.com>
2026-09-23 15:47:59 +0700

    Update API config to local host

--- face_recognition_attendance/lib/core/config/api_config.dart
+++ face_recognition_attendance/lib/core/config/api_config.dart
@@ -5,12 +5,12 @@ class ApiConfig {
   // Set your computer's LAN IP address when testing on a physical mobile device.
   // When running on Android Emulator, use '10.0.2.2'.
   // When running on iOS Simulator or Web/Desktop, use '127.0.0.1'.
-  static String serverHost = '10.0.2.2';
+  static String serverHost = '127.0.0.1';
   static int serverPort = 8000;
 
   /// Returns the base URL for the Django REST API (v1).
-  // static String get baseUrl => 'http://$serverHost:$serverPort/api/v1';
-  static String get baseUrl => 'https://alive-ultimately-bathroom-forgotten.trycloudflare.com/api/v1';
+  static String get baseUrl => 'http://$serverHost:$serverPort/api/v1';
+  // static String get baseUrl => 'https://alive-ultimately-bathroom-forgotten.trycloudflare.com/api/v1';
 
   /// Request timeout
   static const Duration timeoutDuration = Duration(seconds: 30);
